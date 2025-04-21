0b55c6d4e0e8f2b4af1a5b24d52a87ba521ab3e3
tawana <tawanaworks@gmail.com>
2025-04-21 17:15:52 +0200

    feat: add 16x16 grid of squares

---
+++ scripts/sketch.js
@@ -0,0 +1,9 @@
+const canvas = document.querySelector("#canvas");
+const CANVAS_SIZE = 16;
+const SIZE_FACTOR = 16; // change it so it recives input from webpage
+
+for (let i = 0; i < CANVAS_SIZE * SIZE_FACTOR; i++) {
+    let tile = document.createElement("div");
+    tile.textContent += i + 1;
+    canvas.appendChild(tile);
+}
