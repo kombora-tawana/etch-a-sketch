f9a33c496265a3bf454a77440764778a41cf81bb
tawana <tawanaworks@gmail.com>
2025-04-23 17:07:17 +0200

    feat: toggles tile color on hover
    when the mouse hovers over a tile this will activate the tiles color,
    whilst the tile is hovered the color remains active. when the mouse
    leaves the tile the color deactivates after 5 seconds

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -4,6 +4,14 @@ const SIZE_FACTOR = 16; // change it so it recives input from webpage
 
 for (let i = 0; i < CANVAS_SIZE * SIZE_FACTOR; i++) {
     let tile = document.createElement("div");
-    tile.textContent += i + 1;
+    // tile.textContent += i + 1;
+    tile.addEventListener("mouseenter", () => {
+        tile.classList.add("hover");
+    });
+    tile.addEventListener("mouseleave", () => {
+        setTimeout(() => {
+            tile.classList.remove("hover");
+        }, 5000);
+    });
     canvas.appendChild(tile);
 }
