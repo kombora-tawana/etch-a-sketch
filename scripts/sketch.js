77cda5d1915754c4059fef0b5b13d8ece618812d
tawana <tawanaworks@gmail.com>
2025-05-05 21:48:43 +0200

    refactor: change the selector for choosing the canvas

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,17 +1,19 @@
-const canvas = document.querySelector("#canvas");
+const canvas = document.querySelector(".canvas");
 const CANVAS_SIZE = 16;
 const SIZE_FACTOR = 16; // change it so it recives input from webpage
 
 for (let i = 0; i < CANVAS_SIZE * SIZE_FACTOR; i++) {
     let tile = document.createElement("div");
+
     // tile.textContent += i + 1;
+
     tile.addEventListener("mouseenter", () => {
         tile.classList.add("hover");
     });
     tile.addEventListener("mouseleave", () => {
         setTimeout(() => {
             tile.classList.remove("hover");
-        }, 5000);
+        }, 1500);
     });
     canvas.appendChild(tile);
 }
