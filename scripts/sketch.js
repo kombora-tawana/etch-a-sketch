8945a65ec7195d946330db025a564e88002bdf97
tawana.zw <130402485+kombora-tawana@users.noreply.github.com>
2026-03-16 22:19:00 +0200

    feat: add css transtions to bgc change

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,47 +1,47 @@
 const canvas = document.querySelector(".canvas");
 
 function generateSketchPad(size_factor = 16) {
-    canvas.innerHTML = ""; // clear previous tiles
+  canvas.innerHTML = ""; // clear previous tiles
 
-    // canvas size
-    const canvasWidth = canvas.clientWidth;
-    const canvasHeight = canvas.clientHeight;
+  // canvas size
+  const canvasWidth = canvas.clientWidth;
+  const canvasHeight = canvas.clientHeight;
 
-    // tile length
-    const tileWidth = canvasWidth / size_factor;
-    const tileHeight = canvasHeight / size_factor;
+  // tile length
+  const tileWidth = canvasWidth / size_factor;
+  const tileHeight = canvasHeight / size_factor;
 
-    for (let i = 0; i < size_factor * size_factor; i++) {
-        let tile = document.createElement("div");
-        tile.style.width = `${tileWidth}px`;
-        tile.style.height = `${tileHeight}px`;
+  for (let i = 0; i < size_factor * size_factor; i++) {
+    let tile = document.createElement("div");
+    tile.style.width = `${tileWidth}px`;
+    tile.style.height = `${tileHeight}px`;
 
-        // tile.textContent += i + 1; // commented out because the numbers are a debugging tool which helps you see where the boxes are
+    // tile.textContent += i + 1; // commented out because the numbers are a debugging tool which helps you see where the boxes are
 
-        tile.addEventListener("mouseenter", () => {
-            tile.classList.add("hover");
-        });
-        tile.addEventListener("mouseleave", () => {
-            setTimeout(() => {
-                tile.classList.remove("hover");
-            }, 7500);
-        });
-        canvas.appendChild(tile);
-    }
+    tile.addEventListener("pointerenter", () => {
+      tile.classList.add("hover");
+    });
+    tile.addEventListener("pointerleave", () => {
+      setTimeout(() => {
+        tile.classList.remove("hover");
+      }, 7500);
+    });
+    canvas.appendChild(tile);
+  }
 }
 
 const sizeBtn = document.querySelector(".sizeBtn");
 sizeBtn.addEventListener("click", () => {
-    let size = Number(prompt("Enter a number between 1-100"));
-    while (isNaN(size) || size < 1 || size > 100) {
-        size = Number(prompt("Invalid. Enter a number between 1-100"));
-    }
+  let size = Number(prompt("Enter a number between 1-100"));
+  while (isNaN(size) || size < 1 || size > 100) {
+    size = Number(prompt("Invalid. Enter a number between 1-100"));
+  }
 
-    const sketchTxt = document.querySelector("section > p");
-    sketchTxt.textContent = `sketchpad : [ ${size} x ${size} ]`;
+  const sketchTxt = document.querySelector("section > p");
+  sketchTxt.textContent = `sketchpad : [ ${size} x ${size} ]`;
 
-    generateSketchPad(size);
-    console.log(`SIZE_FACTOR == ${size_factor}`);
+  generateSketchPad(size);
+  console.log(`SIZE_FACTOR == ${size_factor}`);
 });
 
 generateSketchPad();
